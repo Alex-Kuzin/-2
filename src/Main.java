aafbc5b30b3f6d2d52c59001e16ceef403d96ac3
Alex Kuzin <acidkarma3000@gmail.com>
2023-05-02 16:18:25 +0500

    Format code

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-         int balance = 8000;
+        int balance = 8000;
         int replenishment_1 = 500;
-        int balance_2 = balance  + replenishment_1;
-         int points;
-  if (balance_2 > 1000)
-  {
-      points = (balance_2 - 1000) / 100;
-  }
-else  {
-      points = 0;
-  }
-    System.out.println("Баланс:" + balance_2 + " " + "Кол-во бонусов:" + points);
-
+        int balance_2 = balance + replenishment_1;
+        int points;
+        if (balance_2 > 1000) {
+            points = (balance_2 - 1000) / 100;
+        } else {
+            points = 0;
         }
+        System.out.println("Баланс:" + balance_2 + " " + "Кол-во бонусов:" + points);
 
     }
+
+}
